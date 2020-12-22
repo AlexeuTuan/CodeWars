b746862f231cd5701119beb9435fbf15969f765d
AlexeyTyan <alekseytyan45@gmail.com>
2020-12-22 19:52:35 +0300

    Greed, a dice game

--- src/main/java/com/company/Greed.java
+++ src/main/java/com/company/Greed.java
@@ -2,6 +2,8 @@ package com.company;
 
 
 /**
+ * GREED
+ *
  * Greed is a dice game played with five six-sided dice.
  * Your mission, should you choose to accept it,
  * is to score a throw according to these rules. You will
