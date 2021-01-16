c9aa9966de75d3d7dbd37bf24f6f6bf47fa894a8
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-16 14:58:06 +0300

    TimeConversion

--- src/main/java/alekseytyan/Main.java
+++ src/main/java/alekseytyan/Main.java
@@ -3,12 +3,13 @@ package alekseytyan;
 import alekseytyan.hackerrank.DiagonalDifference;
 import alekseytyan.hackerrank.PlusMinus;
 import alekseytyan.hackerrank.StairCase;
+import alekseytyan.hackerrank.TimeConversion;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        StairCase.staircase(6);
+        System.out.println(TimeConversion.timeConversion("12:00:01AM"));
     }
 }
