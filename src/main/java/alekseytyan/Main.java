c7b155b27cdbd106dab10c009981e41a89aff525
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-16 12:49:50 +0300

    PlusMinus

--- src/main/java/alekseytyan/Main.java
+++ src/main/java/alekseytyan/Main.java
@@ -1,22 +1,13 @@
 package alekseytyan;
 
 import alekseytyan.hackerrank.DiagonalDifference;
+import alekseytyan.hackerrank.PlusMinus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<List<Integer>> arr = new ArrayList<>();
-
-        for (int i = 1; i <= 9; i+=3) {
-            List<Integer> row = new ArrayList<>();
-            row.add(i);
-            row.add(i+1);
-            row.add(i+2);
-            arr.add(row);
-        }
-
-        System.out.println(DiagonalDifference.diagonalDifference(arr));
+        PlusMinus.plusMinus(new int[] {1,2,-1,-2,0});
     }
 }
