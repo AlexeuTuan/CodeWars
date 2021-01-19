887b52b7ea715a29719693ce4d84fccd57c248e1
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-19 13:43:07 +0300

    Apple and Orange

--- src/main/java/alekseytyan/Main.java
+++ src/main/java/alekseytyan/Main.java
@@ -2,17 +2,12 @@ package alekseytyan;
 
 import alekseytyan.hackerrank.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(73);
-        list.add(67);
-        list.add(38);
-        list.add(33);
-
-        System.out.println(GradingStudents.gradingStudents(list));
+        AppleAndOrange.countApplesAndOranges(
+                7,11,
+                5, 15,
+                new int[] {-2, 2, 1},
+                new int[] {5,-6});
     }
 }
