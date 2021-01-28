2cb09d494438744a2d6456026a11aa79f602d433
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-28 13:15:18 +0300

    2D Arrays-DS

--- src/main/java/alekseytyan/Main.java
+++ src/main/java/alekseytyan/Main.java
@@ -1,11 +1,19 @@
 package alekseytyan;
 
 import alekseytyan.hackerrank.datastructures.ArraysDS;
+import alekseytyan.hackerrank.datastructures.TwoDArraysDS;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(ArraysDS.reverseArray(new int[]{1, 2, 3, 5})));
+        int[][] sample = {{1,1,1,0,0,0},
+                {0,1,0,0,0,0},
+                {1,1,1,0,0,0},
+                {0,0,2,4,4,0},
+                {0,0,0,2,0,0},
+                {0,0,1,2,4,0}};
+
+        System.out.println(TwoDArraysDS.hourglassSum(sample));
     }
 }
