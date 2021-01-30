e1bbf9d54e56284a47bdf4bbd6ca2488334a75f1
AlexeyTyan <alekseytyan45@gmail.com>
2021-01-30 16:51:54 +0300

    PaginationHelper : To complete

--- src/main/java/alekseytyan/Main.java
+++ src/main/java/alekseytyan/Main.java
@@ -1,9 +1,16 @@
 package alekseytyan;
 
-import alekseytyan.codewars.ColouredTriangles;
+import alekseytyan.codewars.PaginationHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ColouredTriangles.triangle("RBRGBRB"));
+        PaginationHelper<Integer> helper = new PaginationHelper<>(new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9)), 4);
+
+        helper.pageIndex(3);
+        helper.pageItemCount(3);
+
     }
 }
---
+++ src/main/java/alekseytyan/codewars/PaginationHelper.java
@@ -0,0 +1,77 @@
+package alekseytyan.codewars;
+
+import java.util.List;
+
+/**
+ * For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+ *
+ * The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+ *
+ * The following are some examples of how this class is used:
+ *
+ * PaginationHelper<Character> helper = new PaginationHelper(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'), 4);
+ * helper.pageCount(); //should == 2
+ * helper.itemCount(); //should == 6
+ * helper.pageItemCount(0); //should == 4
+ * helper.pageItemCount(1); // last page - should == 2
+ * helper.pageItemCount(2); // should == -1 since the page is invalid
+ *
+ * // pageIndex takes an item index and returns the page that it belongs on
+ * helper.pageIndex(5); //should == 1 (zero based index)
+ * helper.pageIndex(2); //should == 0
+ * helper.pageIndex(20); //should == -1
+ * helper.pageIndex(-10); //should == -1
+ * @param <I> - type of list element
+ */
+public class PaginationHelper<I> {
+
+    private List<I> collection;
+    private int itemsPerPage;
+    private int pageCount;
+
+    /**
+     * The constructor takes in an array of items and a integer indicating how many
+     * items fit within a single page
+     */
+    public PaginationHelper(List<I> collection, int itemsPerPage) {
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
+
+        pageCount = collection.size() / itemsPerPage + 1;
+        System.out.println(pageCount);
+    }
+
+    /**
+     * returns the number of items within the entire collection
+     */
+    public int itemCount() {
+        return collection.size();
+    }
+
+    /**
+     * returns the number of pages
+     */
+    public int pageCount() {
+        return pageCount;
+    }
+
+    /**
+     * returns the number of items on the current page. page_index is zero based.
+     * this method should return -1 for pageIndex values that are out of range
+     */
+    public int pageItemCount(int pageIndex) {
+        if(pageIndex > pageCount || pageIndex < 0 || collection.isEmpty()) return -1;
+        else if(pageIndex == pageCount) return collection.size() - (pageCount - 1) * itemsPerPage;
+        else return itemsPerPage;
+    }
+
+    /**
+     * determines what page an item is on. Zero based indexes
+     * this method should return -1 for itemIndex values that are out of range
+     */
+    public int pageIndex(int itemIndex) {
+        if(collection.isEmpty() || itemIndex >= collection.size() || itemIndex < 0) return -1;
+        int result = itemIndex / itemsPerPage;
+        return itemIndex % itemsPerPage == 0 ? result : result + 1;
+    }
+}
